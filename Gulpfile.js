3865a2fd290b0aa867b3abf4f1a2a56b639533d0
Antti Mattila <anttti@fastmail.fm>
2015-04-04 17:50:00 +0300

    Use Gulp to handle styles

---
+++ Gulpfile.js
@@ -0,0 +1,16 @@
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+  gulp.src('./_sass/*.scss')
+    .pipe(sass({ errLogToConsole: true }))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./sass/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch'], function() {});
